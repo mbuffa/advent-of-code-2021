d885fc2dc426c1680c61475dfdf43dee31617535
Maxime Buffa <mbuffa@gmail.com>
2021-12-03 15:34:38 +0100

    Add Day2 Part One

---
+++ lib/day2.ex
@@ -0,0 +1,33 @@
+defmodule Day2 do
+  defmodule Submarine do
+    defstruct [:position, :depth]
+  end
+
+  def run(filename) do
+    File.read!(filename)
+    |> String.split("\n")
+    |> Enum.reject(& &1 == "")
+    |> Enum.reduce(%Submarine{position: 0, depth: 0}, fn instruction, submarine ->
+      execute_instruction(submarine, instruction)
+    end)
+    |> compute_multiple()
+  end
+
+  defp execute_instruction(submarine, instruction)
+  defp execute_instruction(submarine, "forward " <> x) do
+    n = String.to_integer(x)
+    %Submarine{submarine | position: submarine.position + n}
+  end
+  defp execute_instruction(submarine, "up " <> x) do
+    n = String.to_integer(x)
+    %Submarine{submarine | depth: submarine.depth - n}
+  end
+  defp execute_instruction(submarine, "down " <> x) do
+    n = String.to_integer(x)
+    %Submarine{submarine | depth: submarine.depth + n}
+  end
+
+  def compute_multiple(%Submarine{position: position, depth: depth}) do
+    position * depth
+  end
+end
---
+++ test/day2_test.exs
@@ -0,0 +1,9 @@
+defmodule Day2Test do
+  use ExUnit.Case
+
+  describe "part_one" do
+    test "example" do
+      assert Day2.run("data/2/example.txt") == 150
+    end
+  end
+end
